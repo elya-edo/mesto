154cab97fe91b56151c0f0f987b89d0cd6934aef
Eleonora Rakhmanova <elya-edo@yandex.ru>
2023-05-26 15:36:03 +0300

    fix: внесены правки

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,36 @@
-let editProfileButton = document.querySelector(".profile__edit-button"); // кнопка редактирования
-let Popup = document.querySelector(".popup"); // окно попапа
-let closePopupButton = document.querySelector(".popup__close-button"); // кнопка закрытия попапа
+let editProfileButton = document.querySelector('.profile__edit-button'); // кнопка редактирования
+let popup = document.querySelector('.popup'); // окно попапа
+let closePopupButton = document.querySelector('.popup__close-button'); // кнопка закрытия попапа
 
-let nameInput = document.querySelector("#name-input"); // поле для имени
-let descriptionInput = document.querySelector("#description-input"); // поле для описания
-let nameProfile = document.querySelector(".profile__name"); // имя профиля описание профиля
-let descriptionProfile = document.querySelector(".profile__description"); // описание профиля
+let nameInput = document.querySelector('#name-input'); // поле для имени
+let descriptionInput = document.querySelector('#description-input'); // поле для описания
+let nameProfile = document.querySelector('.profile__name'); // имя профиля описание профиля
+let descriptionProfile = document.querySelector('.profile__description'); // описание профиля
 
-let PopupForm = document.querySelector(".popup__form"); // форма
+let popupForm = document.querySelector('.popup__form'); // форма
 
-editProfileButton.addEventListener("click", function () {
-  Popup.classList.add("popup_opened");
-});
+editProfileButton.addEventListener('click', openPopup);
 
-closePopupButton.addEventListener("click", function () {
-  Popup.classList.remove("popup_opened");
-});
+closePopupButton.addEventListener('click', closePopup);
 
-// заполняю форму при открытии данными из профиля
-nameInput.value = nameProfile.textContent;
-descriptionInput.value = descriptionProfile.textContent;
+popupForm.addEventListener('submit', editPopup);
 
-PopupForm.addEventListener("submit", function (evt) {
+
+
+function openPopup(){
+  popup.classList.add('popup_opened');
+  // заполняю форму при открытии данными из профиля
+  nameInput.value = nameProfile.textContent;
+  descriptionInput.value = descriptionProfile.textContent;
+}
+
+function closePopup(){
+  popup.classList.remove('popup_opened');
+}
+
+function editPopup(evt){
   evt.preventDefault();
   nameProfile.textContent = nameInput.value;
   descriptionProfile.textContent = descriptionInput.value;
-  Popup.classList.remove("popup_opened");
-});
+  popup.classList.remove('popup_opened');
+}
