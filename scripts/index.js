c70fc1b81891291e9c2f4b5971a7506816ff3ba2
Eleonora Rakhmanova <elya-edo@yandex.ru>
2023-05-27 18:01:23 +0300

    fix: внесены правки

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,17 @@
 let editProfileButton = document.querySelector('.profile__edit-button'); // кнопка редактирования
 let popup = document.querySelector('.popup'); // окно попапа
 let closePopupButton = document.querySelector('.popup__close-button'); // кнопка закрытия попапа
-
 let nameInput = document.querySelector('#name-input'); // поле для имени
 let descriptionInput = document.querySelector('#description-input'); // поле для описания
 let nameProfile = document.querySelector('.profile__name'); // имя профиля описание профиля
 let descriptionProfile = document.querySelector('.profile__description'); // описание профиля
-
 let popupForm = document.querySelector('.popup__form'); // форма
-
-editProfileButton.addEventListener('click', openPopup);
-
-closePopupButton.addEventListener('click', closePopup);
-
-popupForm.addEventListener('submit', editPopup);
-
 
 
 function openPopup(){
   popup.classList.add('popup_opened');
-  // заполняю форму при открытии данными из профиля
-  nameInput.value = nameProfile.textContent;
-  descriptionInput.value = descriptionProfile.textContent;
+  nameInput.value = nameProfile.textContent;               // заполняю форму при открытии данными из профиля
+  descriptionInput.value = descriptionProfile.textContent; // заполняю форму при открытии данными из профиля
 }
 
 function closePopup(){
@@ -32,5 +22,10 @@ function editPopup(evt){
   evt.preventDefault();
   nameProfile.textContent = nameInput.value;
   descriptionProfile.textContent = descriptionInput.value;
-  popup.classList.remove('popup_opened');
+  closePopup();
 }
+
+
+editProfileButton.addEventListener('click', openPopup);
+closePopupButton.addEventListener('click', closePopup);
+popupForm.addEventListener('submit', editPopup);
